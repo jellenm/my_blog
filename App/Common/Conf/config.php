51e8a0dcf4e16993dfa20f6d91e8cbf01135f09c
jellenm@126.com <jellenm@126.com>
2018-03-19 22:35:23 +0800

    本地数据库

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -4,10 +4,14 @@ return array(
 
     //数据库配置
     'DB_TYPE'=>'mysql',
-    'DB_USER'=>'bdm258386228',
-    'DB_PWD'=>'lei456789',
-    'DB_NAME'=>'bdm258386228_db',
-    'DB_HOST'=>'bdm258386228.my3w.com',
+//    'DB_USER'=>'bdm258386228',
+//    'DB_PWD'=>'lei456789',
+//    'DB_NAME'=>'bdm258386228_db',
+//    'DB_HOST'=>'bdm258386228.my3w.com',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'',
+    'DB_NAME'=>'jellenweb',
+    'DB_HOST'=>'127.0.0.1',
     'DB_PORT'=>3306,
     'DB_PREFIX'=>'think_',
     'DB_CHARSET'=>'utf8',
